a4e7acfa148fadfc7b9d5a2741c2deb7185491dc
jimzers <adamleesaintfrancis@gmail.com>
2019-03-18 20:30:24 -0700

    init book table and annotation table

--- app.py
+++ app.py
@@ -13,10 +13,31 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(80), unique=True)
+    user_library = db.Column(db.String(65535))
+    follow_list = db.Column(db.String(65535))
 
     def __init__(self, username, email):
         self.username = username
         self.email = email
+
+class Book(db.Model):
+    __tablename__ = "books"
+
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.TEXT(65535))
+    chapters = db.Column(db.Integer)
+    has_access = db.Column(db.String(65535))
+
+
+class Annotation(db.Model):
+    __tablename__ = "annotations"
+
+    id = db.Column(db.Integer, primary_key=True)
+    book_id = db.Column(db.Integer, db.ForeignKey(Book.id), primary_true=True)
+    text
+    range_
+
+
 
 @app.route('/')
 def hello_world():
